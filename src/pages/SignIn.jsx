4d6aaf25bfbba4fe45ea5da182195a754c9c2b6e
Cliemtor Fabros <cliemtor08@gmail.com>
2022-10-22 09:19:32 +0800

    Update sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,11 +9,12 @@ import React from 'react'
 import axios from 'axios'
 import getAccount from '../api/getAccount'
 import { navigate } from '@reach/router'
+import { toast } from 'react-toastify'
 
 function SignIn() {
   // SEND GET ACCOUNT REQUEST
-  // const has_token = localStorage.getItem('qikstarter-token') ? true : false
-  // const Account = getAccount(has_token)
+  const has_token = localStorage.getItem('q-agri-web-token') ? true : false
+  const Account = getAccount(has_token)
 
   // INFORMATION STATE
   const [status, setStatus] = React.useState('success')
@@ -24,52 +25,51 @@ function SignIn() {
   const [password, setPassword] = React.useState('admin123')
 
   // ON FETCH ACCOUNT
-  // React.useEffect(() => {
-  //   if (has_token && Account.loading) setStatus('loading')
-  //   if (Account.error) setStatus('error')
-  //   if (Account.data) setStatus('success')
-  //   return () => setStatus('loading')
-  // }, [Account.loading, Account.error, Account.data])
+  React.useEffect(() => {
+    if (has_token && Account.loading) setStatus('loading')
+    if (Account.error) setStatus('error')
+    if (Account.data) setStatus('success')
+    return () => setStatus('loading')
+  }, [Account.loading, Account.error, Account.data])
 
   // SEND POST LOGIN REQUEST
   function submitForm(e) {
     e.preventDefault()
-    // setStatus('loading')
+    setStatus('loading')
 
-    // const URL = process.env.BASE_URL + '/login'
-    // const DATA = { email: email, password: password }
+    const URL = process.env.BASE_URL + '/login'
+    const DATA = { email: email, password: password }
 
-    // axios
-    //   .post(URL, DATA)
-    //   .then((response) => {
-    //     setStatus('success')
-    //     if (response.status === 201) {
-    //       localStorage.setItem('qikstarter-token', response.headers['x-token'])
-    //       toast.success('Welcome ' + response?.data?.name)
-    navigate('/settlement-area/landslide', { replace: true })
-    //   }
-    // })
-    // .catch((error) => {
-    //   setStatus('success')
-    //   if (error.response) {
-    //     if (error.response?.status === 400) toast.error('Form input is invalid')
-    //     else if (error.response?.status === 401) toast.error('Email and/or password is incorrect')
-    //     else if (error.response?.status === 403) toast.error('Your account was deactivated by admin')
-    //     else if (error.response?.status === 500) toast.error('Unexpected server error')
-    //   } else if (error.request) console.error(error.request)
-    //   else console.error('Error', error.message)
-    // })
+    axios
+      .post(URL, DATA)
+      .then((response) => {
+        setStatus('success')
+        if (response.status === 201) {
+          localStorage.setItem('q-agri-web-token', response.headers['x-token'])
+          toast.success('Welcome ' + response?.data?.name)
+          navigate('/settlement-area/landslide', { replace: true })
+        }
+      })
+      .catch((error) => {
+        setStatus('success')
+        if (error.response) {
+          if (error.response?.status === 400) toast.error('Form input is invalid')
+          else if (error.response?.status === 401) toast.error('Email and/or password is incorrect')
+          else if (error.response?.status === 403) toast.error('Your account was deactivated by admin')
+          else if (error.response?.status === 500) toast.error('Unexpected server error')
+        } else if (error.request) console.error(error.request)
+        else console.error('Error', error.message)
+      })
   }
 
   return (
     <FadeAnimation>
       <div className="sign-in-container">
-        {/* {Account.data && <Redirect to="/farmers/dashboard" noThrow replace />} */}
+        {Account.data && <Redirect to="/settlement-area/landslide" noThrow replace />}
         <div className="sign-in-content">
           <img className="sign-in-image" src={require('../assets/sign_in_bg.svg')} alt="poster" />
           <form className="sign-in-form" onSubmit={submitForm} status={status}>
-            <div className="sign-in-title text-orange">Qikstarter</div>
-            {/* <p className="sign-in-note">By signing in, you agree to our Terms of Use and Privacy Policy.</p> */}
+            <div className="sign-in-title text-orange">Q-Agri MIS</div>
             <Field label="Email">
               <Input className="sign-in-input" onChange={(e) => setEmail(e.target.value)} required size="35" type="email" value={email} />
             </Field>
@@ -84,6 +84,7 @@ function SignIn() {
                 value={password}
               />
             </Field>
+            <p className="sign-in-note">By signing in, you agree to our Terms of Use and Privacy Policy.</p>
             <Button className="sign-in-button" disabled={status === 'loading'} loadingText="Signing in..." type="submit" status={status}>
               Sign In
             </Button>
