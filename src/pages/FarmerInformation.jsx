c4023fc1b50c2e0540120a2e9d5a50b6bb964d55
Cliemtor Fabros <cliemtor08@gmail.com>
2022-10-31 09:16:41 +0800

    Extract pulsing dot to new js file

--- src/pages/FarmerInformation.jsx
+++ src/pages/FarmerInformation.jsx
@@ -23,6 +23,7 @@ import axios from 'axios'
 import { confirmAlert } from 'react-confirm-alert'
 import getFarmerById from '../api/getFarmerById'
 import getFarms from '../api/getFarms'
+import mapMarker from '../mapMarker'
 import mapboxgl from 'mapbox-gl'
 import { toast } from 'react-toastify'
 
@@ -97,8 +98,7 @@ function FarmerInformation() {
           Crosshair: true,
           PrintableArea: true,
           accessToken: process.env.MAPBOX_ACCESS_TOKEN
-        }),
-        'top-right'
+        })
       )
 
       map.on('idle', () => {
@@ -108,58 +108,7 @@ function FarmerInformation() {
   }, [map])
 
   React.useEffect(() => {
-    // PULSING DOT
-    const size = 150
-    const pulsingDot = {
-      width: size,
-      height: size,
-      data: new Uint8Array(size * size * 4),
-
-      // When the layer is added to the map,
-      // get the rendering context for the map canvas.
-      onAdd: function () {
-        const canvas = document.createElement('canvas')
-        canvas.width = this.width
-        canvas.height = this.height
-        this.context = canvas.getContext('2d')
-      },
-
-      // Call once before every frame where the icon will be used.
-      render: function () {
-        const duration = 1000
-        const t = (performance.now() % duration) / duration
-
-        const radius = (size / 2) * 0.3
-        const outerRadius = (size / 2) * 0.7 * t + radius
-        const context = this.context
-
-        // Draw the outer circle.
-        context.clearRect(0, 0, this.width, this.height)
-        context.beginPath()
-        context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2)
-        context.fillStyle = `rgba(255, 200, 200, ${1 - t})`
-        context.fill()
-
-        // Draw the inner circle.
-        context.beginPath()
-        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2)
-        context.fillStyle = 'rgba(255, 100, 100, 1)'
-        context.strokeStyle = 'white'
-        context.lineWidth = 2 + 4 * (1 - t)
-        context.fill()
-        context.stroke()
-
-        // Update this image's data with data from the canvas.
-        this.data = context.getImageData(0, 0, this.width, this.height).data
-
-        // Continuously repaint the map, resulting
-        // in the smooth animation of the dot.
-        map.triggerRepaint()
-
-        // Return `true` to let the map know that the image was updated.
-        return true
-      }
-    }
+    const pulsingDot = mapMarker(100, map)
 
     if (map && Farmer.data) {
       console.log('COORDINATES: ' + Farmer.data.address_latitude + ', ' + Farmer.data.address_longitude)
@@ -195,9 +144,7 @@ function FarmerInformation() {
             }
           })
         }
-      })
 
-      map.on('load', () => {
         map.addSource('mapbox-dem', {
           'type': 'raster-dem',
           'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
